f32ba1b8e2a737c74141051ea23e4f96ac47a081
ShubhangDesai <shubhang.desai@gmail.com>
2016-12-05 11:26:07 -0800

    add dataset, clean graph, and execute unrollings in session

--- src/model.py
+++ src/model.py
@@ -1,151 +1,189 @@
 from __future__ import print_function
-import os
 import numpy as np
-import random
-import string
 import tensorflow as tf
+import collections
 import zipfile
+import re
 from six.moves import range
-from six.moves.urllib.request import urlretrieve
 
-k = 15000
+k = 150
+num_batches = 1000
+
+def read_data(filename):
+    z = zipfile.ZipFile(filename, 'r')
+    lines = list()
+    line_count = 0
+    with z.open(z.namelist()[0]) as f:
+        for line in f:
+            line_count += 1
+            line = line.decode('utf-8')
+            lines.append(line.replace(".", "").replace("?", "").replace("!", ""))
+            if line_count == num_batches:
+                break
+    z.close()
+    return lines
+
+french = read_data('fr.zip')
+english = read_data('en.zip')
+print('Data size %d %d' % (len(english), len(french)))
+
+def build_dataset(sentences):
+  count = [['UNK', -1]]
+  count.extend(collections.Counter(re.findall(r'\w+', ' '.join(sentences).lower())).most_common(k - 1))
+  indeces = dict()
+  for word, _ in count:
+    indeces[word] = len(indeces)
+  data = list()
+  unk_count = 0
+  for sentence in sentences:
+    words = re.findall(r'[\w]+', sentence.lower())
+    sentence_arr = np.zeros((k, 0))
+    for word in words:
+        if word in indeces:
+            index = indeces[word]
+        else:
+            index = 0
+            unk_count += 1
+        vector = np.zeros((k, 1))
+        vector[index] = 1
+        sentence_arr = np.concatenate((sentence_arr, vector), axis=1)
+    data.append(sentence_arr)
+  count[0][1] = unk_count
+  return data, count, indeces
+
+fr_data, fr_count, fr_indeces = build_dataset(french)
+print('Most common words (+UNK)', fr_count[:5])
+en_data, en_count, en_indeces = build_dataset(english)
+print('Most common words (+UNK)', en_count[:5])
+del english, french  # Hint to reduce memory.
+
+dim_embedding = 50
+dim_hidden = 100
 
 graph = tf.Graph()
 with graph.as_default():
     # Variables
-    # Encoder input
-    N = tf.placeholder(tf.int32, name="N") #TODO: "extract" value from here
-    X = tf.placeholder(tf.float32, shape=[N, k])
-    we = tf.Variable(tf.truncated_normal([500, k], -0.1, 0.1))
+    # Encoder embedding
+    we = tf.Variable(tf.truncated_normal([dim_embedding, k], -0.1, 0.1))
     # Encoder update gate
-    wz = tf.Variable(tf.truncated_normal([1000, 500], -0.1, 0.1))
-    uz = tf.Variable(tf.truncated_normal([1000, 1000], -0.1, 0.1))
+    wz = tf.Variable(tf.truncated_normal([dim_hidden, dim_embedding], -0.1, 0.1))
+    uz = tf.Variable(tf.truncated_normal([dim_hidden, dim_hidden], -0.1, 0.1))
     # Encoder reset gate
-    wr = tf.Variable(tf.truncated_normal([1000, 500], -0.1, 0.1))
-    ur = tf.Variable(tf.truncated_normal([1000, 1000], -0.1, 0.1))
+    wr = tf.Variable(tf.truncated_normal([dim_hidden, dim_embedding], -0.1, 0.1))
+    ur = tf.Variable(tf.truncated_normal([dim_hidden, dim_hidden], -0.1, 0.1))
     # Encoder h~ [find name]
-    w = tf.Variable(tf.truncated_normal([1000, 500], -0.1, 0.1))
-    u = tf.Variable(tf.truncated_normal([1000, 1000], -0.1, 0.1))
+    w = tf.Variable(tf.truncated_normal([dim_hidden, dim_embedding], -0.1, 0.1))
+    u = tf.Variable(tf.truncated_normal([dim_hidden, dim_hidden], -0.1, 0.1))
     # Encoder representation weight
-    v = tf.Variable(tf.truncated_normal([1000, 1000], -0.1, 0.1))
+    v = tf.Variable(tf.truncated_normal([dim_hidden, dim_hidden], -0.1, 0.1))
     # Decoder representation weight
-    v_prime = tf.Variable(tf.truncated_normal([1000, 1000], -0.1, 0.1))
-    # Decoder input
-    w_prime_e = tf.Variable(tf.truncated_normal([500, k], -0.1, 0.1))
+    v_prime = tf.Variable(tf.truncated_normal([dim_hidden, dim_hidden], -0.1, 0.1))
+    # Decoder embedding
+    w_prime_e = tf.Variable(tf.truncated_normal([dim_embedding, k], -0.1, 0.1))
     # Decoder update gate
-    w_prime_z = tf.Variable(tf.truncated_normal([1000, 500], -0.1, 0.1))
-    u_prime_z = tf.Variable(tf.truncated_normal([1000, 1000], -0.1, 0.1))
-    Cz = tf.Variable(tf.truncated_normal([1000, 1000], -0.1, 0.1))
+    w_prime_z = tf.Variable(tf.truncated_normal([dim_hidden, dim_embedding], -0.1, 0.1))
+    u_prime_z = tf.Variable(tf.truncated_normal([dim_hidden, dim_hidden], -0.1, 0.1))
+    Cz = tf.Variable(tf.truncated_normal([dim_hidden, dim_hidden], -0.1, 0.1))
     # Decoder reset gate
-    w_prime_r = tf.Variable(tf.truncated_normal([1000, 500], -0.1, 0.1))
-    u_prime_r = tf.Variable(tf.truncated_normal([1000, 1000], -0.1, 0.1))
-    Cr = tf.Variable(tf.truncated_normal([1000, 1000], -0.1, 0.1))
+    w_prime_r = tf.Variable(tf.truncated_normal([dim_hidden, dim_embedding], -0.1, 0.1))
+    u_prime_r = tf.Variable(tf.truncated_normal([dim_hidden, dim_hidden], -0.1, 0.1))
+    Cr = tf.Variable(tf.truncated_normal([dim_hidden, dim_hidden], -0.1, 0.1))
     # Decoder h~ [find name]
-    w_prime = tf.Variable(tf.truncated_normal([1000, 500], -0.1, 0.1))
-    u_prime = tf.Variable(tf.truncated_normal([1000, 1000], -0.1, 0.1))
-    C = tf.Variable(tf.truncated_normal([1000, 1000], -0.1, 0.1))
+    w_prime = tf.Variable(tf.truncated_normal([dim_hidden, dim_embedding], -0.1, 0.1))
+    u_prime = tf.Variable(tf.truncated_normal([dim_hidden, dim_hidden], -0.1, 0.1))
+    C = tf.Variable(tf.truncated_normal([dim_hidden, dim_hidden], -0.1, 0.1))
     # Decoder maxout calculation
-    oh = tf.Variable(tf.truncated_normal([2000, 1000], -0.1, 0,1))
-    oy = tf.Variable(tf.truncated_normal([2000, 1000], -0.1, 0, 1))
-    oc = tf.Variable(tf.truncated_normal([2000, 1000], -0.1, 0, 1))
+    oh = tf.Variable(tf.truncated_normal([2 * dim_hidden, dim_hidden], -0.1, 0, 1))
+    oy = tf.Variable(tf.truncated_normal([2 * dim_hidden, k], -0.1, 0, 1))
+    oc = tf.Variable(tf.truncated_normal([2 * dim_hidden, dim_hidden], -0.1, 0, 1))
     # Decoder output
-    gl = tf.Variable(tf.truncated_normal([k, 500], -0.1, 0, 1))
-    gr = tf.Variable(tf.truncated_normal([500, 1000], -0.1, 0, 1))
+    gl = tf.Variable(tf.truncated_normal([k, dim_embedding], -0.1, 0, 1))
+    gr = tf.Variable(tf.truncated_normal([dim_embedding, dim_hidden], -0.1, 0, 1))
 
     # Encoder
-    h_previous = tf.zeros([1000])
-    for t in range(N):
-        # Current vector and its embedding
-        xt = tf.reshape(tf.slice(N, [t, 0], [1, k]), [k])
-        e = tf.matmul(we, xt)
-        # Vectors for reset calculation
-        wr_e = tf.matmul(wr, e)
-        ur_ht_previous = tf.matmul(ur, h_previous)
-        # Vectors for update calculation
-        wz_e = tf.matmul(wz, e)
-        uz_ht_previous = tf.matmul(wz, h_previous)
+    # TODO: h_previous = tf.zeros([k, 1])
+    # TODO: xt = tf.reshape(X[:, t], [k, 1])
+    h_previous = tf.placeholder(tf.float32, shape=[dim_hidden, 1])
+    xt = tf.placeholder(tf.float32, shape=[k, 1])
+    # Current vector and its embedding
+    e = tf.matmul(we, xt)
+    # Reset calculation
+    r = tf.sigmoid(tf.matmul(wr, e) + tf.matmul(ur, h_previous))
+    # Update calculation
+    z = tf.sigmoid(tf.matmul(wz, e) + tf.matmul(uz, h_previous))
+    # Hidden-tilde calculation
+    h_tilde = tf.tanh(tf.matmul(w, e) + tf.matmul(u, r * h_previous))
+    # Hidden calculation
+    one = tf.ones([dim_hidden, 1])
+    h = z * h_previous + (one - z) * h_tilde
 
-        # Reset calculation
-        r = tf.zeros([1000])
-        for j in range(1000):
-            j = j + 1
-            rj = tf.sigmoid(tf.slice(wr_e, [j], [1]) + tf.slice(ur_ht_previous, [j], [1]))
-            r = r + tf.sparse_tensor_to_dense(tf.SparseTensor([j], [rj], [1000]))
-
-        # Vectors for h~ calculation
-        w_e = tf.matmul(w, e)
-        r_ewm_h_previous = tf.zeros([1000])
-        for j in range (1000):
-            j = j + 1
-            ewm = tf.slice(r, [j], [1]) * tf.slice(h_previous, [j], [1])
-            r_ewm_h_previous = r_ewm_h_previous + tf.sparse_tensor_to_dense(tf.SparseTensor([j], [ewm], [1000]))
-        u_r_ewm_h_previous = tf.matmul(u, r_ewm_h_previous)
-
-        # Hidden calculation
-        h = tf.zeros([1000])
-        for j in range(1000):
-            j = h + 1
-            #Update calculation
-            zj = tf.sigmoid(tf.slice(wz_e, [j], [1]) + tf.slice(uz_ht_previous, [j], [1]))
-            #h~ calculation
-            hj_tilde = tf.tanh(tf.slice(w_e, [j], [1]) + tf.slice(u_r_ewm_h_previous, [j], [1]))
-
-            hj = zj*tf.slice(h_previous, [j], [1]) + (1-zj)*hj_tilde
-            h = h + tf.sparse_tensor_to_dense(tf.SparseTensor([j], [hj], [1000]))
-
-        h_previous = h
+    # Summary calculation
     c = tf.tanh(tf.matmul(v, h_previous))
+    h_prime_init = tf.tanh(tf.matmul(v_prime, c))
 
     # Decoder
-    continue_sentence = True
-    y_previous = tf.zeros([k])
-    h_prime_previous = tf.tanh(tf.matmul(v_prime, c))
-    while (continue_sentence): #TODO: while y_previous is not EOS
-        # Current vector's embedding
-        e = tf.matmul(w_prime_e, y_previous)
-        # Vectors for h~ calculation
-        w_e = tf.matmul(w_prime, e)
-        u_h_previous = tf.matmul(u_prime, h_prime_previous)
-        C_c = tf.matmul(C, c)
-        # Vectors for reset calculation
-        wr_e = tf.matmul(w_prime_r, e)
-        ur_ht_previous = tf.matmul(u_prime_r, h_previous)
-        Cr_c = tf.matmul(Cr, c)
-        # Vectors for update calculation
-        wz_e = tf.matmul(w_prime_z, e)
-        uz_ht_previous = tf.matmul(w_prime_z, h_previous)
-        Cz_c = tf.matmul(Cz, c)
+    # TODO: y_previous = tf.zeros([k, 1])
+    # TODO: h_prime_previous = tf.tanh(tf.matmul(v_prime, c))
+    y_previous = tf.placeholder(tf.float32, shape=[k, 1])
+    h_prime_previous = tf.placeholder(tf.float32, shape=[dim_hidden, 1])
+    # Current vector's embedding
+    e = tf.matmul(w_prime_e, y_previous)
+    # Reset calculation
+    r_prime = tf.sigmoid(tf.matmul(w_prime_r, e) + tf.matmul(u_prime_r, h_prime_previous) + tf.matmul(Cr, c))
+    # Update calculation
+    z_prime = tf.sigmoid(tf.matmul(w_prime_z, e) + tf.matmul(u_prime_z, h_prime_previous) + tf.matmul(Cz, c))
+    # Hidden-tilde calculation
+    h_tilde_prime = tf.tanh(tf.matmul(w_prime, e) + r_prime * (tf.matmul(u_prime, h_prime_previous) + tf.matmul(C, c)))
+    # Hidden calculation
+    one = tf.ones([dim_hidden, 1])
+    h_prime = z_prime * h_prime_previous + (one - z_prime) * h_tilde_prime
+    # Maxout calculation
+    s_prime = tf.matmul(oh, h_prime) + tf.matmul(oy, y_previous) + tf.matmul(oc, c)
+    s = tf.reshape(tf.reduce_max(tf.reshape(s_prime, [dim_hidden, 2]), 1), [dim_hidden, 1])
+    # Logit calculation
+    g = tf.matmul(gl, gr)
+    logit = tf.matmul(g, s)
 
-        # Hidden calculation
-        h_prime = tf.zeros([1000])
-        for j in range(1000):
-            j = j + 1
-            r_prime_j = tf.sigmoid(tf.slice(wr_e, [j], [1]) + tf.slice(u_h_previous, [j], [1])
-                                   + tf.slice(Cr_c, [j], 1))
-            z_prime_j = tf.sigmoid(tf.slice(wz_e, [j], [1]) + tf.slice(uz_ht_previous, [j], [1])
-                                   + tf.slice(Cz_c, [j], [1]))
-            h_prime_j_tilde = tf.tanh(tf.slice(w_e, [j], [1]) + r_prime_j * tf.slice(u_h_previous + C_c, [j], [1]))
-            h_prime_j = z_prime_j*tf.slice(h_prime_previous, [j], [1]) + (1-z_prime_j)*h_prime_j_tilde
-            h_prime = h_prime + tf.sparse_tensor_to_dense(tf.SparseTensor([j], [h_prime_j], [1000]))
+    # Optimizer
+    logits = tf.placeholder(tf.float32, shape=[k, None])
+    Y = tf.placeholder(tf.float32, shape=[k, None])
+    global_step = tf.Variable(0)
+    learning_rate = tf.train.exponential_decay(10.0, global_step, 5000, 0.1, staircase=True)
+    loss = tf.nn.softmax_cross_entropy_with_logits(logits, Y)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss) # TODO: figure out what's going wrong
+    tf.add(global_step, 1)
 
-        # Maxout calculation
-        s_prime = tf.matmul(oh, h_prime) + tf.matmul(oy, y_previous) + tf.matmul(oc, c)
-        s = tf.zeros([1000])
-        for i in range(1000):
-            i = i + 1
-            s_i = max(tf.slice(s, [2*i-1], [1]), tf.slice(s, [2*i], [1])) #TODO: fix this, this is almost certainly wrong
-            s = s + tf.sparse_tensor_to_dense(tf.SparseTensor([i], [s_i], [1000]))
+sess = tf.Session(graph=graph)
 
-        # Softmax calculation
-        g = tf.matmul(gl, gr)
-        denominator = tf.zeros([1])
-        softmax = tf.zeros([k])
-        for j in range(k):
-            j = j + 1
-            g_j = tf.reshape(tf.slice(g, [j, 0], [1, 1000]), [1, 1000])
-            e_g_j_s = tf.exp(tf.matmul(g_j, s))
-            softmax = softmax + tf.sparse_tensor_to_dense(tf.SparseTensor([j], [e_g_j_s], [1000]))
-            denominator = denominator + tf.sparse_tensor_to_dense(tf.SparseTensor([1], [e_g_j_s], [1000]))
-        softmax = tf.matmul(softmax, tf.inverse(denominator))
+with sess:
+    tf.initialize_all_variables().run()
+    print("Initialized")
+    for step in range(num_batches):
+        # Get input and target matrices
+        input = en_data.pop()
+        target = fr_data.pop()
 
-        h_prime_previous = h_prime
+        # Run encoder
+        h_prev = np.zeros([dim_hidden, 1])
+        for t in range(input.shape[1]):
+            h_prev = sess.run([h], feed_dict={h_previous: h_prev, xt: input[:, t].reshape((k, 1))})
+            h_prev = np.asarray(h_prev).reshape((dim_hidden, 1))
+
+        # Run decoder
+        h_prime_prev = sess.run([h_prime_init], feed_dict={h_previous: h_prev})
+        y_prev = np.zeros([k, 1])
+        logit_list = list()
+        for t in range(target.shape[1]):
+            h_prime_prev = np.asarray(h_prime_prev).reshape((dim_hidden, 1))
+            print("Shape: %s; Dtype: %s" % (h_prime_prev.shape, h_prime_prev.dtype))
+            # TODO: figure out what's going wrong
+            h_prime_prev, logit_current = sess.run([h_prime, logit], feed_dict={y_previous: y_prev, h_prime_previous: h_prime_prev})
+            print("test")
+            logit_current = np.asarray(logit_current).reshape(k, 1)
+            y_prev = target[:, t].reshape((k, 1))
+            logit_list.append(logit_current)
+
+        # Run optimization
+        _, l = sess.run([optimizer, loss], feed_dict={logits: logit_list, Y: target})
+        if (step+1) % 100 == 0:
+            print("Loss at step %d: %f" % (step, l))
